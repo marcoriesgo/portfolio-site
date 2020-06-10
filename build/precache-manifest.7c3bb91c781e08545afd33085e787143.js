9548abd96e3dc8fa21064ead6a3e47ea31c16566
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-06-10 11:17:11 -0700

    build

---
+++ build/precache-manifest.7c3bb91c781e08545afd33085e787143.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63cfadf00717a64dc0c1c776dd36a56f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b3e82af7ad256a7a105",
+    "url": "/static/css/main.c8e8e4fe.chunk.css"
+  },
+  {
+    "revision": "e156439a3ad50d555f2d",
+    "url": "/static/js/2.5a6d20bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5a6d20bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3e82af7ad256a7a105",
+    "url": "/static/js/main.480a2a49.chunk.js"
+  },
+  {
+    "revision": "12c006469facf3d5ce64",
+    "url": "/static/js/runtime-main.9a184eb6.js"
+  },
+  {
+    "revision": "4dab5a90b413b1e10b88699347e7dfde",
+    "url": "/static/media/chronos.4dab5a90.png"
+  },
+  {
+    "revision": "32e916cc8967738e71f8f455369988ca",
+    "url": "/static/media/electrum.32e916cc.png"
+  },
+  {
+    "revision": "1941ed465c0ca9aa90c59ff479ea108f",
+    "url": "/static/media/profile.1941ed46.jpg"
+  },
+  {
+    "revision": "d6f3f14fe2e4cd79b4f69cd2eba76b31",
+    "url": "/static/media/recipes.d6f3f14f.png"
+  },
+  {
+    "revision": "f7b2bb1f50a7e60014a13282908451cc",
+    "url": "/static/media/remembr.f7b2bb1f.png"
+  }
+]);
